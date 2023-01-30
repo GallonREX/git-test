7bed1bc4c8d451b9ee1e0a574bef92bea60c2865
zoujialun <zoujialun@peopleyun.cn>
2023-01-30 14:17:58 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Author: zoujialun
+ * @Date: 2023/01/30/14:09
+ * @version:
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
